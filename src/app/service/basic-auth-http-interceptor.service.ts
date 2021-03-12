22dc1ff231b2b831137d32a6fa739b60b352dc03
sureshkumarkarnan <80433828+sureshkumarkarnan@users.noreply.github.com>
2021-03-12 14:44:49 +0530

    Create basic-auth-http-interceptor.service.ts

---
+++ src/app/service/basic-auth-http-interceptor.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthHttpInterceptorService implements HttpInterceptor{
+
+  constructor() { }
+
+intercept(req: HttpRequest<any>, next: HttpHandler) {
+  
+}
+}
