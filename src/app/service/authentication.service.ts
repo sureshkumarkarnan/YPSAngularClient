7b120ce22e04a3151e0db1cba50baf18865d7885
sureshkumarkarnan <80433828+sureshkumarkarnan@users.noreply.github.com>
2021-03-12 14:19:07 +0530

    Add files via upload

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from "@angular/core";
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+export class User{
+  constructor(
+    public status:string,
+     ) {}
+  
+}
+
 @Injectable({
   providedIn: "root"
 })
 export class AuthenticationService {
-  constructor() {}
+constructor(    private httpClient:HttpClient ) {      }
 
   authenticate(username, password) {
-    if (username === "suresh" && password === "suresh") {
-      sessionStorage.setItem("username", username);
-      return true;
-    } else {
-      return false;
-    }
+     const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
+    console.log("in autenticate");
+    return this.httpClient.get<User>('http://localhost:8080/login',{headers}).pipe(
+     map(
+       userData => {
+        sessionStorage.setItem('username',username);
+        return userData;
+       }
+     )
+
+    );
   }
   isUserLoggedIn() {
     let user = sessionStorage.getItem("username");
-    console.log(!(user === null));
+   // console.log(!(user === null));
     return !(user === null);
   }
   logOut() {
