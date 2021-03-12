3e2af8cfd79271bc54672f245bbbbe711183ac44
sureshkumarkarnan <80433828+sureshkumarkarnan@users.noreply.github.com>
2021-03-12 12:18:51 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+import { HomeComponent } from "./home/home.component";
+import { StudentComponent } from "./student/student.component";
+import { HttpClientService } from "./service/http-client.service";
+import { LoginComponent } from "./login/login.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { AuthenticationService } from "./service/authentication.service";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    StudentComponent,
+    LoginComponent,
+    LogoutComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule,FormsModule],
+  providers: [HttpClientService, AuthenticationService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
