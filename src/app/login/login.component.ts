e93634e59610091c3bf6536f601dce375a79fa5a
sureshkumarkarnan <80433828+sureshkumarkarnan@users.noreply.github.com>
2021-03-18 14:40:51 +0530

    Add files via upload

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthenticationService } from "../service/authentication.service";
-
+import { first } from 'rxjs/operators';
 @Component({
   selector: "login",
   templateUrl: "./login.component.html",
@@ -9,22 +9,37 @@ import { AuthenticationService } from "../service/authentication.service";
 })
 export class LoginComponent implements OnInit {
   newcomponent = "Entered in loginComponent created";
+  currentUser: any;
   username;
   password;
   invalidLogin = false;
-
+loading = false;
+error:string;
   constructor(
     private router: Router,
     private loginService: AuthenticationService
-  ) {}
+  ) {
+	   this.loginService.currentUser.subscribe(x => this.currentUser = x);
+  }
   ngOnInit() {}
 
   checkLogin() {
-    if (this.loginService.authenticate(this.username, this.password)) {
-      this.router.navigate([""]);
-      this.invalidLogin = false;
-    } else {
-      this.invalidLogin = true;
-    }
+	   this.loading = true;
+   this.loginService.authenticate(this.username, this.password)
+   .pipe(first())
+            .subscribe(
+                data => {
+                     console.log("checkLogin success");
+					  this.router.navigate(['/']);
+					  this.invalidLogin = false;
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+					console.log("checkLogin failed");
+					this.invalidLogin = true;
+                });
+     
+    } 
   }
-}
+
